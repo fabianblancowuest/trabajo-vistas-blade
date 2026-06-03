6c1fc3bf1db49b9ffb5e242eb6fe86c1475a8bc7
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2026-06-03 08:49:04 -0300

    Agregar una nueva página llamada Nosotros

---
+++ resources/views/nosotros.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('titulo', 'Nosotros')
+@section('contenido')
+    <h2>Nosotros</h2>
+    <p>Somos una empresa dedicada a la venta de productos online.</p>
+    <p>Nuestra misión es ofrecer productos de calidad a precios competitivos.</p>
+    <p>Contamos con un equipo de profesionales comprometidos con la satisfacción de nuestros clientes.</p>
+    <p>¡Gracias por visitarnos!</p>
+@endsection
